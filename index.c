c120c88395b63a09bd7c5f13443b666e6ee28c33
szaif <aryenzaifrani@gmail.com>
2021-12-18 12:19:25 +0530

    today i am learning github

---
+++ index.c
@@ -0,0 +1,11 @@
+ Volume in drive C has no label.
+ Volume Serial Number is EC84-D99F
+
+ Directory of C:\Users\USER\OneDrive\Desktop\MyProject
+
+18-12-2021  12:12    <DIR>          .
+18-12-2021  12:12    <DIR>          ..
+18-12-2021  12:12                 0 index.c
+18-12-2021  12:12               357 index.html
+               2 File(s)            357 bytes
+               2 Dir(s)  164,416,434,176 bytes free
---
+++ index.js
@@ -0,0 +1,14 @@
+ Volume in drive C has no label.
+ Volume Serial Number is EC84-D99F
+
+ Directory of C:\Users\USER\OneDrive\Desktop\MyProject
+
+18-12-2021  12:12    <DIR>          .
+18-12-2021  12:12    <DIR>          ..
+18-12-2021  12:12               402 index.c
+18-12-2021  12:12               495 index.css
+18-12-2021  12:12               357 index.html
+18-12-2021  12:12                 0 index.js
+18-12-2021  12:12               448 index.py
+               5 File(s)          1,702 bytes
+               2 Dir(s)  164,416,303,104 bytes free
